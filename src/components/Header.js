4e136d6f5876edac3501f0b6ff96ca7a1f906400
Morgan Bradford <morganjbradford95@gmail.com>
2021-05-03 17:29:02 -0500

    add bootstrap. Add TopBar, Logo, Links, Carousel to header. Conditional display TODO

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,29 @@
 import React from "react";
+import Carousel from "./Carousel";
+import Links from "./Links";
+import Logo from "./Logo";
+import TopBar from "./TopBar";
 
 function Header() {
-  return (
-    <h1>Legacy</h1>
-  );
+  // TODO: check state slice responsible for Carousel|| hook?
+  if (stateCheck) {
+    return (
+      <>
+        <TopBar />
+        <Logo />
+        <Links />
+        <Carousel />
+      </>
+    );
+  } else {
+    return (
+      <>
+        <TopBar />
+        <Logo />
+        <Links />
+      </>
+    );
+  }
 }
 
 export default Header;
