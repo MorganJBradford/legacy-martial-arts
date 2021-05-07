875966697dc2ca786962a7199d9b4c804ad3d22d
Morgan Bradford <morganjbradford95@gmail.com>
2021-05-07 17:42:27 -0500

    add placeholder columns to test styling

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,8 +1,27 @@
 import React from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function MainContent() {
   return(
-    <h2>Body</h2>
+    <div className="mainContent">
+      <div className="container">
+        <Row>
+          <Col className="col">
+            <h1 className="ourClasses">Our Classes <button className="ourClassesBtn">View All</button></h1>
+          </Col>
+          <Col className="col">Bleep</Col>
+          <Col className="col">Blp</Col>
+        </Row>
+        <Row>
+          <Col>Bloop2</Col>
+          <Col>Bloop2</Col>
+        </Row>
+        <Row>
+          <Col>Bloop3</Col>
+        </Row>
+      </div>
+    </div>
   );
 }
 
