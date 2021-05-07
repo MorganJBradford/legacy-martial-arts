66c68e4f6a0f31c0e3a28ec44ea85d41c04074a3
Morgan Bradford <morganjbradford95@gmail.com>
2021-05-07 12:29:10 -0500

    add div.TopBar; div.TopBarBoxList; div.TopBarBoxLi. Add hours and phone number to li

---
+++ src/components/TopBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function TopBar() {
+  return (
+    <div className="topBar">
+      <div className="container">
+        <ul className="topBarBoxList">
+          <li className="topBarBoxLi">Hours</li>
+          <li className="topBarBoxLi">(630) 224-0580</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default TopBar;
