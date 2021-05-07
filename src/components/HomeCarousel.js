7785d91d0a7035256d9ff03a946073da1e8ca76a
Morgan Bradford <morganjbradford95@gmail.com>
2021-05-07 15:26:40 -0500

    add react-bootstrap carousel with photos from Legacy's facebook

---
+++ src/components/HomeCarousel.js
@@ -0,0 +1,37 @@
+import React from "react";
+import Carousel from "react-bootstrap/Carousel";
+import Instructors from "../img/instructors.jpg";
+import MJ from "../img/mj.jpg";
+import WolfPack from "../img/wolf-pack.jpg"
+
+function HomeCarousel() {
+  return (
+    <Carousel>
+    <Carousel.Item>
+      <img
+        className="d-block w-100 carousel container"
+        src={MJ}
+        alt="Master Jane"
+      />
+    </Carousel.Item>
+
+    <Carousel.Item>
+      <img
+        className="d-block w-100 carousel container"
+        src={WolfPack}
+        alt="Legacy Martial Arts Wolf Pack"
+      />
+    </Carousel.Item>
+
+    <Carousel.Item>
+      <img
+        className="d-block w-100 carousel container"
+        src={Instructors}
+        alt="Master Jane and instructors"
+      />
+    </Carousel.Item>
+  </Carousel>
+  );
+}
+
+export default HomeCarousel;
