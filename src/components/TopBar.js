08b2a4fe809c3578431546bfd89ce94810bf4a5a
Morgan Bradford <morganjbradford95@gmail.com>
2021-05-07 13:06:28 -0500

    add ul for social media links

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -4,9 +4,13 @@ function TopBar() {
   return (
     <div className="topBar">
       <div className="container">
-        <ul className="topBarBoxList">
-          <li className="topBarBoxLi">Hours</li>
-          <li className="topBarBoxLi">(630) 224-0580</li>
+        <ul className="topBarList">
+          <li className="topBarLi">Opening Hours</li>
+          <li className="topBarLi">(630) 224-0580</li>
+        </ul>
+        <ul className="topBarSocialsList">
+          <li className="topBarSocialsLi"><a href="https://www.facebook.com/Legacy.TKD" class="fa fa-facebook"></a></li>
+          <li className="topBarSocialsLi"><a href="https://twitter.com/legacytkd?lang=en" class="fa fa-twitter"></a></li>
         </ul>
       </div>
     </div>
