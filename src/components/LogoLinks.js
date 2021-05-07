39e9564c8099fb276405216aedd370c589c4e53c
Morgan Bradford <morganjbradford95@gmail.com>
2021-05-07 18:42:08 -0500

    separate items via row/column to make mobile friendly

--- src/components/LogoLinks.js
+++ src/components/LogoLinks.js
@@ -1,19 +1,27 @@
 import React from "react";
 import LegacyLogo from "../img/legacy-logo.jpg";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 
 function Logo() {
   return (
     <div className="container">
-      <img src={LegacyLogo} id="legacyLogo" alt="Legacy Martial Arts Logo"/>
-      <div className="linksDiv">
-        <ul className="linksList">
-          <li className="linksLi">HOME</li>
-          <li className="linksLi">ABOUT</li>
-          <li className="linksLi">CLASSES</li>
-          <li className="linksLi">FAQ</li>
-          <li className="linksLi linksLiLast">STUDENTS</li>
-        </ul>
-      </div>
+      <Row>
+        <Col md={2}>
+          <img src={LegacyLogo} id="legacyLogo" alt="Legacy Martial Arts Logo"/>
+        </Col>
+        <Col md={10}>
+          <div className="linksDiv">
+            <ul className="linksList">
+              <li className="linksLi">HOME</li>
+              <li className="linksLi">ABOUT</li>
+              <li className="linksLi">CLASSES</li>
+              <li className="linksLi">FAQ</li>
+              <li className="linksLi linksLiLast">STUDENTS</li>
+            </ul>
+          </div>
+        </Col>
+      </Row>
     </div>
   );
 }
