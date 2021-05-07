676812fed7b5d3a08938cc12f64fa04c83856eda
Morgan Bradford <morganjbradford95@gmail.com>
2021-05-07 13:19:50 -0500

    add instagram, calendar, and phone icons. Add instagram link

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -5,12 +5,13 @@ function TopBar() {
     <div className="topBar">
       <div className="container">
         <ul className="topBarList">
-          <li className="topBarLi">Opening Hours</li>
-          <li className="topBarLi">(630) 224-0580</li>
+          <li className="topBarLi"><i class="fa fa-calendar"></i>Opening Hours</li>
+          <li className="topBarLi"><i class="fa fa-phone"></i>(630) 224-0580</li>
         </ul>
         <ul className="topBarSocialsList">
           <li className="topBarSocialsLi"><a href="https://www.facebook.com/Legacy.TKD" class="fa fa-facebook"></a></li>
           <li className="topBarSocialsLi"><a href="https://twitter.com/legacytkd?lang=en" class="fa fa-twitter"></a></li>
+          <li className="topBarSocialsLi"><a href="https://www.instagram.com/tkdgirl307/?hl=en" class="fa fa-instagram"></a></li>
         </ul>
       </div>
     </div>
