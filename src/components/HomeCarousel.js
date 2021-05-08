49b4bde934f9dd6350db3f3b1a0bb6dff7b2bf90
Morgan Bradford <morganjbradford95@gmail.com>
2021-05-08 17:39:44 -0500

    correct indentation

--- src/components/HomeCarousel.js
+++ src/components/HomeCarousel.js
@@ -7,30 +7,30 @@ import WolfPack from "../img/wolf-pack.jpg"
 function HomeCarousel() {
   return (
     <Carousel>
-    <Carousel.Item>
-      <img
-        className="d-block w-100 carousel container"
-        src={MJ}
-        alt="Master Jane"
-      />
-    </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100 carousel container"
+          src={MJ}
+          alt="Master Jane"
+        />
+      </Carousel.Item>
 
-    <Carousel.Item>
-      <img
-        className="d-block w-100 carousel container"
-        src={WolfPack}
-        alt="Legacy Martial Arts Wolf Pack"
-      />
-    </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100 carousel container"
+          src={WolfPack}
+          alt="Legacy Martial Arts Wolf Pack"
+        />
+      </Carousel.Item>
 
-    <Carousel.Item>
-      <img
-        className="d-block w-100 carousel container"
-        src={Instructors}
-        alt="Master Jane and instructors"
-      />
-    </Carousel.Item>
-  </Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-100 carousel container"
+          src={Instructors}
+          alt="Master Jane and instructors"
+        />
+      </Carousel.Item>
+    </Carousel>
   );
 }
 
