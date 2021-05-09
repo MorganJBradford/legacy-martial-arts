e175f01dbf4ff40cefcb3af364b3cc3ab8a4c390
Morgan Bradford <morganjbradford95@gmail.com>
2021-05-09 00:09:45 -0500

    add div section. Define col sizes

--- src/components/ClassCol.js
+++ src/components/ClassCol.js
@@ -1,17 +1,24 @@
 import React from "react";
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
+import { Col, Row } from "react-bootstrap";
+import EmKick from "../img/em-rhkick.jpg";
 
 function ClassCol() {
   return (
-    <Col xs={12} md={4} className="mainCols">
-      <div className="sectionWrapper">
-        <Row className="colHeader">
-          <Col><h1 className="colH1">OUR CLASSES</h1></Col>
-          <Col><button className="colBtns">View All</button></Col>
-        </Row>
-      </div>
-    </Col>
+    <>
+      <Col xs={12} md={4} lg={4} xl={4} className="mainCols">
+        <div className="headerWrapper d-flex justify-content-center">
+          <Row className="colHeader">
+            <Col><h1 className="colH1">OUR CLASSES</h1></Col>
+            <Col><button className="colBtns">View All</button></Col>
+          </Row>
+        </div>
+          <div className="sectionWrapper d-flex justify-content-center">
+            <Row className="colSection">
+              <Col><img src={EmKick} className="colPic"/></Col>
+            </Row>
+          </div>
+      </Col>
+    </>
   );
 }
 
