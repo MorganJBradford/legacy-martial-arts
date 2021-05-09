7d88411ba75d1ca138fae8d81d904208489d720c
Morgan Bradford <morganjbradford95@gmail.com>
2021-05-09 00:10:15 -0500

    add col sizes. justify content center

--- src/components/InstructorCol.js
+++ src/components/InstructorCol.js
@@ -1,11 +1,10 @@
 import React from "react";
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
+import { Col, Row } from "react-bootstrap";
 
 function InstructorCol() {
   return (
-    <Col xs={12} md={4} className="mainCols">
-      <div className="sectionWrapper">
+    <Col xs={12} md={4} lg={4} xl={4} className="mainCols">
+      <div className="headerWrapper d-flex justify-content-center">
         <Row className="colHeader">
           <Col><h1 className="colH1">OUR INSTRUCTORS</h1></Col>
           <Col><button className="colBtns">View All</button></Col>
