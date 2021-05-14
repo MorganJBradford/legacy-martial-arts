63d5c679dfd6515a963231f808cc8e77280ffca9
Morgan Bradford <morganjbradford95@gmail.com>
2021-05-14 17:49:40 -0500

    add logo, add hours column

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,12 +2,13 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
-import FootColOne from "./ClassCol";
-import FootColTwo from "./InstructorCol";
-import FootColThree from "./NewsCol";
+import Logo from "../img/legacy-logo.jpg";
 
 function Footer() {
   return (
+    <>
+    <div className="footerBuffer">
+    </div>
     <div className="footerBackground">
       <Container>
         <Row>
@@ -35,9 +36,10 @@ function Footer() {
           </Col>
           <Col xs={12} md={4} lg={4} xl={4} className="mainCols">
             <div className="sectionWrapper d-flex justify-content-center">
-              <Row className="footerColSection">
-                <Col><h2 className="footerColH2">OUR INSTRUCTORS</h2></Col>
-                <Col><p className="footerData">View All</p></Col>
+              <Row>
+                <Col>
+                  <img src={Logo} id="footerLogo"/>
+                </Col>
               </Row>
             </div>
           </Col>
@@ -46,17 +48,33 @@ function Footer() {
             <div className="sectionWrapper d-flex justify-content-center">
               <Row className="footerColSection">
                 <Col>
-                  <h2 className="footerColH2">LEGACY MARTIAL ARTS</h2>
+                  <h2 className="footerColH2">OPENING HOURS</h2>
                   <Row className="locationSection">
-                    <Col><h2 className="footerColH2">ADDRESS</h2></Col>
+                    <Col><h2 className="footerColH2">MONDAY</h2></Col>
                     <Col><p className="footerLocation">blah blah blah</p></Col>
                   </Row>
                   <Row className="locationSectionMid">
-                    <Col><h2 className="footerColH2">TELEPHONE</h2></Col>
+                    <Col><h2 className="footerColH2">TUESDAY</h2></Col>
                     <Col><p className="footerLocation">View All</p></Col>
                   </Row>
                   <Row className="locationSection">
-                    <Col><h2 className="footerColH2">EMAIL</h2></Col>
+                    <Col><h2 className="footerColH2">WEDNESDAY</h2></Col>
+                    <Col><p className="footerLocation">View All</p></Col>
+                  </Row>
+                  <Row className="locationSectionMid">
+                    <Col><h2 className="footerColH2">THURSDAY</h2></Col>
+                    <Col><p className="footerLocation">View All</p></Col>
+                  </Row>
+                  <Row className="locationSection">
+                    <Col><h2 className="footerColH2">FRIDAY</h2></Col>
+                    <Col><p className="footerLocation">View All</p></Col>
+                  </Row>
+                  <Row className="locationSectionMid">
+                    <Col><h2 className="footerColH2">SATURDAY</h2></Col>
+                    <Col><p className="footerLocation">View All</p></Col>
+                  </Row>
+                  <Row className="locationSection">
+                    <Col><h2 className="footerColH2">SUNDAY</h2></Col>
                     <Col><p className="footerLocation">View All</p></Col>
                   </Row>
                 </Col>
@@ -66,6 +84,7 @@ function Footer() {
         </Row>
       </Container>
     </div>
+    </>
   );
 }
 
