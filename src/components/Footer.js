dd34068f6b049f52156219cbcb1b0e519c9435b2
Morgan Bradford <morganjbradford95@gmail.com>
2021-05-14 15:38:53 -0500

    add footer template

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,52 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import FootColOne from "./ClassCol";
+import FootColTwo from "./InstructorCol";
+import FootColThree from "./NewsCol";
 
 function Footer() {
   return (
-    <footer>Footer</footer>
+    <div className="footerBackground">
+      <Container>
+        <Row>
+          <Col xs={12} md={4} lg={4} xl={4} className="mainCols">
+            <div className="sectionWrapper d-flex justify-content-center">
+              <Row className="classColSection">
+                <Col>
+                  <h1 className="classColH1">FAMILY TAEKWONDO</h1>
+                  <p className="classColP">Keep your family strong while having a great time together.</p>
+                  <button className="classColBtn">MORE INFO</button>
+                </Col>
+              </Row>
+            </div>
+          </Col>
+          <Col xs={12} md={4} lg={4} xl={4} className="mainCols">
+            <div className="sectionWrapper d-flex justify-content-center">
+              <Row className="classColSection">
+                <Col>
+                  <h1 className="classColH1">FAMILY TAEKWONDO</h1>
+                  <p className="classColP">Keep your family strong while having a great time together.</p>
+                  <button className="classColBtn">MORE INFO</button>
+                </Col>
+              </Row>
+            </div>
+          </Col>
+          <Col xs={12} md={4} lg={4} xl={4} className="mainCols">
+            <div className="sectionWrapper d-flex justify-content-center">
+              <Row className="classColSection">
+                <Col>
+                  <h1 className="classColH1">FAMILY TAEKWONDO</h1>
+                  <p className="classColP">Keep your family strong while having a great time together.</p>
+                  <button className="classColBtn">MORE INFO</button>
+                </Col>
+              </Row>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 }
 
