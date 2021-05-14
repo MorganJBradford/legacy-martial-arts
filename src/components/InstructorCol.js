c915c08ee0433ad62ddc7b16e17ec6b3c66607ee
Morgan Bradford <morganjbradford95@gmail.com>
2021-05-14 14:13:04 -0500

    add instructor col placeholder

--- src/components/InstructorCol.js
+++ src/components/InstructorCol.js
@@ -1,17 +1,57 @@
 import React from "react";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
+import EmKick from "../img/em-rhkick.jpg"
+import MJ from "../img/mj1.jpg";
 
 function InstructorCol() {
   return (
-    <Col xs={12} md={4} lg={4} xl={4} className="mainCols">
-      <div className="headerWrapper d-flex justify-content-center">
-        <Row className="colHeader">
-          <Col><h1 className="colH1">OUR INSTRUCTORS</h1></Col>
-          <Col><button className="colBtns">View All</button></Col>
-        </Row>
-      </div>
-    </Col>
+    <>
+      <Col xs={12} md={4} lg={4} xl={4} className="mainCols">
+        <div className="headerWrapper d-flex justify-content-center">
+          <Row className="colHeader">
+            <Col><h1 className="colH1">OUR INSTRUCTORS</h1></Col>
+            <Col><button className="colBtns">View All</button></Col>
+          </Row>
+        </div>
+        <div className="sectionWrapper d-flex justify-content-center">
+          <Row className="instructorColSection">
+            <Col>
+              <img src={MJ} className="colPic"/>
+            </Col>
+            <Col>
+              <h1 className="instructorColH1">MASTER JANE</h1>
+              <p className="classColP">Taekwondo, Hapkido</p>
+              <button className="classColBtn">MORE INFO</button>
+            </Col>
+          </Row>
+        </div>
+        <div className="sectionWrapper d-flex justify-content-center">
+          <Row className="instructorColSection">
+            <Col>
+              <img src={EmKick} className="colPic"/>
+            </Col>
+            <Col>
+              <h1 className="instructorColH1">INSTRUCTOR TATI</h1>
+              <p className="classColP">Taekwondo</p>
+              <button className="classColBtn">MORE INFO</button>
+            </Col>
+          </Row>
+        </div>
+        <div className="sectionWrapper d-flex justify-content-center">
+          <Row className="instructorColSection">
+            <Col>
+              <img src={EmKick} className="colPic"/>
+            </Col>
+            <Col>
+              <h1 className="instructorColH1">INSTRUCTOR ETHAN</h1>
+              <p className="classColP">Taekwondo</p>
+              <button className="classColBtn">MORE INFO</button>
+            </Col>
+          </Row>
+        </div>
+      </Col>
+    </>
   );
 }
 
