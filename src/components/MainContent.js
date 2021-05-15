46742ab0c619c7b4fe32ef43037da4445328e7f7
Morgan Bradford <morganjbradford95@gmail.com>
2021-05-14 20:01:43 -0500

    use context for dev purposes

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,22 +1,41 @@
 import Container from 'react-bootstrap/Container'
-import React from "react";
+import React, { useContext } from "react";
 import Row from "react-bootstrap/Row";
+import { AppContext } from "../AppContext";
 import ClassCol from "./ClassCol";
 import InstructorCol from "./InstructorCol";
 import NewsCol from "./NewsCol";
+import KidsTkd from "./KidsTkd";
 
 function MainContent() {
-  return(
-    <div className="mainContent">
-      <Container>
-        <Row>
-          <ClassCol />
-          <InstructorCol />
-          <NewsCol />
-        </Row>
-      </Container>
-    </div>
-  );
+  const { mainControl, setMainControl } = useContext(AppContext);
+
+
+  if (mainControl === "dev") {
+    return (
+      <div className="mainContent">
+        <Container>
+          <h1>yas</h1>
+          <Row>
+            <KidsTkd/>
+            <ClassCol/>
+          </Row>
+        </Container>
+      </div>
+    ); 
+  } else {
+    return (
+      <div className="mainContent">
+        <Container>
+          <Row>
+            <ClassCol />
+            <InstructorCol />
+            <NewsCol />
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default MainContent;
