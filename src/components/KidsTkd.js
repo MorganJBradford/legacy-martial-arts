6f5e02203b9630709ec0343b8d81c38432d1acbf
Morgan Bradford <morganjbradford95@gmail.com>
2021-05-14 20:02:33 -0500

    add and style template for KidsTkd

---
+++ src/components/KidsTkd.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import EmKick from "../img/em-rhkick.jpg";
+
+function KidsTkd() {
+  return (
+    <>
+      <Col xs={12} md={8} lg={8} xl={8} className="kidsCol">
+        <div className="kidsColSectionOne kidsColPosition">
+          <img src={EmKick} className="kidsColPic"/>
+          <h1 className="classColH1">KIDS TAEKWONDO</h1>
+          <p className="classColP">Taekwondo builds healthy kids, improving their physical coordination and self confidence in a positive atmosphere.</p>
+        </div>
+        <div className="kidsColSectionTwo kidsColPosition">
+          <img src={EmKick} className="kidsColPic"/>
+          <h1 className="classColH1">KIDS TAEKWONDO</h1>
+          <p className="classColP">Taekwondo builds healthy kids, improving their physical coordination and self confidence in a positive atmosphere.</p>
+        </div>
+      </Col>
+    </>
+  );
+}
+
+export default KidsTkd;
