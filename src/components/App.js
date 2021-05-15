b4918456cc5b585b6b365b86f076e7216fa18dfe
Morgan Bradford <morganjbradford95@gmail.com>
2021-05-14 20:00:39 -0500

    pass context to app

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,15 @@ import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 import MainContent from "./MainContent";
+import { AppProvider } from "../AppContext";
 
 function App() {
   return (
-    <>
+    <AppProvider>
       <Header />
       <MainContent />
       <Footer />
-    </>
+    </AppProvider>
   );
 }
 
