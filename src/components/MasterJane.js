874de2c2422867e9418ee5217500f81448f44ebc
Morgan Bradford <morganjbradford95@gmail.com>
2021-05-17 16:02:02 -0500

    display MJ's placeholder details

---
+++ src/components/MasterJane.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Col from "react-bootstrap/Col";
+import colHeaderPic from "../img/mj2.jpg";
+
+function MasterJane() {
+  return (
+    <>
+      <Col xs={12} md={7} lg={7} xl={7} className="kidsCol">
+        <div className="kidsColSectionOne kidsColPosition">
+          <img src={colHeaderPic} className="instructorsColPic"/>
+          <h1 className="kidsColH1">Master Jane Dillon</h1>
+        </div>
+        <div className="kidsColSectionTwo kidsColPosition">
+          <ul className="instructorColUl">
+            <li>5th Degree Black Belt Taekwondo</li>
+          </ul>
+        </div>
+      </Col>
+    </>
+  );
+}
+
+export default MasterJane;
