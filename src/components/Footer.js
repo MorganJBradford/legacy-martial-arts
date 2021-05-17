c5573bac6f4f0052b95181b6bad1ec7ff3b1ac23
Morgan Bradford <morganjbradford95@gmail.com>
2021-05-17 17:16:14 -0500

    correct footer info, style footer info

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -24,11 +24,11 @@ function Footer() {
                   </Row>
                   <Row className="locationSectionMid">
                     <Col><h2 className="footerColH2">TELEPHONE</h2></Col>
-                    <Col><p className="footerLocation">View All</p></Col>
+                    <Col><p className="footerLocation">(630) 224-0580</p></Col>
                   </Row>
                   <Row className="locationSection">
                     <Col><h2 className="footerColH2">EMAIL</h2></Col>
-                    <Col><p className="footerLocation">View All</p></Col>
+                    <Col><p className="footerLocation">masterjane.legacy @gmail.com</p></Col>
                   </Row>
                 </Col>
               </Row>
@@ -50,31 +50,31 @@ function Footer() {
                   <h2 className="footerColH2">OPENING HOURS</h2>
                   <Row className="locationSection">
                     <Col><h2 className="footerColH2">MONDAY</h2></Col>
-                    <Col><p className="footerLocation">blah blah blah</p></Col>
+                    <Col><p className="footerHours">3:30pm - 830:pm</p></Col>
                   </Row>
                   <Row className="locationSectionMid">
                     <Col><h2 className="footerColH2">TUESDAY</h2></Col>
-                    <Col><p className="footerLocation">View All</p></Col>
+                    <Col><p className="footerHours">11:30am - 8:30pm</p></Col>
                   </Row>
                   <Row className="locationSection">
                     <Col><h2 className="footerColH2">WEDNESDAY</h2></Col>
-                    <Col><p className="footerLocation">View All</p></Col>
+                    <Col><p className="footerHours">3:30pm - 830:pm</p></Col>
                   </Row>
                   <Row className="locationSectionMid">
                     <Col><h2 className="footerColH2">THURSDAY</h2></Col>
-                    <Col><p className="footerLocation">View All</p></Col>
+                    <Col><p className="footerHours">3:30pm - 830:pm</p></Col>
                   </Row>
                   <Row className="locationSection">
                     <Col><h2 className="footerColH2">FRIDAY</h2></Col>
-                    <Col><p className="footerLocation">View All</p></Col>
+                    <Col><p className="footerHours">11:30am - 7:30pm</p></Col>
                   </Row>
                   <Row className="locationSectionMid">
                     <Col><h2 className="footerColH2">SATURDAY</h2></Col>
-                    <Col><p className="footerLocation">View All</p></Col>
+                    <Col><p className="footerHours">9:15am - 12:30pm</p></Col>
                   </Row>
                   <Row className="locationSection">
                     <Col><h2 className="footerColH2">SUNDAY</h2></Col>
-                    <Col><p className="footerLocation">View All</p></Col>
+                    <Col><p className="footerHours">CLOSED</p></Col>
                   </Row>
                 </Col>
               </Row>
