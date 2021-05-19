6f1c108a71fa24ce45b3f08642f908bd855d416a
Morgan Bradford <morganjbradford95@gmail.com>
2021-05-18 20:11:18 -0500

    pass in components exlcuding own component into info pages

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -8,10 +8,17 @@ import InstructorCol from "./InstructorCol";
 import NewsCol from "./NewsCol";
 import FamilyTkd from "./FamilyTkd";
 import KidsTkd from "./KidsTkd";
-import MasterJane from "./MasterJane";
-import InstructorJeremy from "./InstructorJeremy";
-import InstructorTati from "./InstructorTati";
+import MasterJaneInfo from "./MasterJane";
+import InstructorJeremyInfo from "./InstructorJeremy";
+import JrInstructorTatiInfo from "./InstructorTati";
 import AdultTkd from "./AdultTkd";
+import ViewClasses from "./ClassesCol/ViewClasses";
+import AdultClass from "./ClassesCol/AdultClass";
+import FamilyClass from "./ClassesCol/FamilyClass";
+import ViewInstructors from "./InstructorsCol/ViewInstructors";
+import InstructorJeremy from "./InstructorsCol/InstructorJeremy";
+import MasterJane from "./InstructorsCol/MasterJane";
+import JrInstructorTati from "./InstructorsCol/JrInstructorTati";
 
 function MainContent() {
   const { mainControl } = useContext(AppContext);
@@ -40,7 +47,15 @@ function MainContent() {
             <KidsTkd/>
             <Col xs={12} md={1} lg={1} xl={1}>
             </Col>
-            <ClassCol/>
+            <Col xs={10} sm={10} md={5} lg={4} xl={4} className="mainCols">
+              <ViewClasses/>
+              <FamilyClass />
+              <AdultClass />
+              <ViewInstructors />
+              <MasterJane />
+              <InstructorJeremy />
+              <JrInstructorTati />
+            </Col>
           </Row>
         </Container>
       </div>
@@ -54,7 +69,15 @@ function MainContent() {
             <FamilyTkd/>
             <Col xs={12} md={1} lg={1} xl={1}>
             </Col>
-            <ClassCol/>
+            <Col xs={10} sm={10} md={5} lg={4} xl={4} className="mainCols">
+              <ViewClasses/>
+              <FamilyClass />
+              <AdultClass />
+              <MasterJane />
+              <ViewInstructors />
+              <InstructorJeremy />
+              <JrInstructorTati />
+            </Col>
           </Row>
         </Container>
       </div>
@@ -68,7 +91,15 @@ function MainContent() {
             <AdultTkd/>
             <Col xs={12} md={1} lg={1} xl={1}>
             </Col>
-            <ClassCol/>
+            <Col xs={10} sm={10} md={5} lg={4} xl={4} className="mainCols">
+              <ViewClasses/>
+              <FamilyClass />
+              <AdultClass />
+              <ViewInstructors />
+              <MasterJane />
+              <InstructorJeremy />
+              <JrInstructorTati />
+            </Col>
           </Row>
         </Container>
       </div>
@@ -79,10 +110,14 @@ function MainContent() {
         <h1 className="kidsColHeader">OUR CLASSES</h1>
         <Container>
           <Row>
-            <MasterJane/>
+            <MasterJaneInfo/>
             <Col xs={12} md={1} lg={1} xl={1}>
             </Col>
-            <InstructorCol/>
+            <Col xs={10} sm={10} md={5} lg={4} xl={4} className="mainCols">
+              <ViewInstructors />
+              <InstructorJeremy />
+              <JrInstructorTati />
+            </Col>
           </Row>
         </Container>
       </div>
@@ -93,10 +128,14 @@ function MainContent() {
         <h1 className="kidsColHeader">OUR CLASSES</h1>
         <Container>
           <Row>
-            <InstructorJeremy/>
+            <InstructorJeremyInfo/>
             <Col xs={12} md={1} lg={1} xl={1}>
             </Col>
-            <InstructorCol/>
+            <Col xs={10} sm={10} md={5} lg={4} xl={4} className="mainCols">
+              <ViewInstructors />
+              <MasterJane />
+              <JrInstructorTati />
+            </Col>
           </Row>
         </Container>
       </div>
@@ -107,10 +146,14 @@ function MainContent() {
         <h1 className="kidsColHeader">OUR CLASSES</h1>
         <Container>
           <Row>
-            <InstructorTati/>
+            <JrInstructorTatiInfo/>
             <Col xs={12} md={1} lg={1} xl={1}>
             </Col>
-            <InstructorCol/>
+            <Col xs={10} sm={10} md={5} lg={4} xl={4} className="mainCols">
+              <ViewInstructors />
+              <MasterJane />
+              <InstructorJeremy />
+            </Col>
           </Row>
         </Container>
       </div>
